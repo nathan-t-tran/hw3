0fb3ae03fef18346c0d3d058bab4044e1a171490
Nathan Tran <nathantt@usc.edu>
2025-03-04 23:27:33 -0800

    llpivot v1

--- llrec.cpp
+++ llrec.cpp
@@ -4,4 +4,31 @@
 // Provide your implementation of llpivot below
 //*********************************************
 
+void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot)
+{
+    //base case - head is null;
+    if (head == nullptr)
+    {
+        return;
+    }
+    else
+    {
+        if (head->val > pivot) //places into larger
+        {
+            Node* temp_head = head;
+            head = NULL;
+            *larger = *temp_head;
 
+            llpivot (temp_head->next, smaller, larger->next, pivot);
+        }
+        else //places into smaller
+        {
+            Node* temp_head = head;
+            head = NULL;
+            *smaller = *temp_head;
+
+            llpivot (temp_head, smaller->next, larger, pivot);
+        }
+    }
+    
+}
